337a49baa70acaef01b45f823feaf1cea9f940e4
embededdrive <embededdrive@naver.com>
2023-01-26 00:08:04 +0900

    230126 민코딩 laptop file update

---
+++ C++/Mincoding/starter_14.5_5.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char sen1[6], sen2[6], arr[11], temp;
+	int size1, size2;
+
+	cin >> sen1 >> sen2;
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (sen1[i] == '\0')
+		{
+			size1 = i;
+			break;
+		}
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		if (sen2[i] == '\0')
+		{
+			size2 = i;
+			break;
+		}
+	}
+
+	for (int i = 0; i < size1; i++)
+	{
+		for (int j = i + 1; j < size1; j++)
+		{
+			if (sen1[i] > sen1[j])
+			{
+				temp = sen1[i];
+				sen1[i] = sen1[j];
+				sen1[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < size2; i++)
+	{
+		for (int j = i + 1; j < size2; j++)
+		{
+			if (sen2[i] > sen2[j])
+			{
+				temp = sen2[i];
+				sen2[i] = sen2[j];
+				sen2[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < size1; i++)
+	{
+		arr[i] = sen1[i];
+	}
+
+	for (int i = 0; i < size2; i++)
+	{
+		arr[size1 + i] = sen2[i];
+	}
+
+	arr[size1 + size2] = '\0';
+
+	for (int i = 0; i < size1 + size2; i++)
+	{
+		cout << arr[i];
+	}
+
+	return 0;
+}
---
+++ C++/Mincoding/starter_14.5_6.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void Magic(int arr[3][3])
+{
+	int num = 1;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = i; j < 3; j++)
+		{
+			arr[i][j] = num++;
+		}
+	}
+}
+
+void Output(int arr[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (arr[i][j] == 0)
+			{
+				cout << ' ';
+			}
+			else {
+				cout << arr[i][j];
+			}
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int arr[3][3] = { 0 };
+
+	Magic(arr);
+	Output(arr);
+
+	return 0;
+}
