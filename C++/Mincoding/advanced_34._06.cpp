3fe4f44f1941d6425e3b5820955aec8a4e160751
embededdrive <embededdirve@naver.com>
2023-02-17 17:55:15 +0900

    230217 minco ~34

---
+++ C++/Mincoding/advanced_34._06.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct Stone {
+	bool isGold;
+	int weight;
+};
+
+bool cmp(Stone left, Stone right){
+	return left.weight > right.weight;
+}
+
+int main()
+{
+	int n;
+	priority_queue<Stone, vector<Stone>, cmp> pq;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+
+		cin >> num;
+
+		pq.push({ true, num });
+	}
+
+	return 0;
+}
