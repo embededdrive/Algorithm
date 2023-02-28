91bd37b63b832e616264309e9dbd99af89bb6157
embededdrive <embededdrive@naver.com>
2023-02-28 23:01:47 +0900

    230228 SWEA 4013

---
+++ C++/SWEA/SWEA_#5656.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct Pos {
+	int y, x;
+};
+
+int n, w, h;
+int map[15][12];
+
+void breakBricks(int idx) {
+
+	queue<Pos> q;
+
+	for (int i = 0; i < h; i++)
+		if (map[i][idx] != 0)
+		{
+			q.push({ i, idx });
+			break;
+		}
+
+
+
+}
+
+int main()
+{
+	int t;
+	
+	cin >> t;
+
+	for (int tc = 1; tc <= t; tc++)
+	{
+		cin >> n >> w >> h;
+
+		for (int i = 0; i < h; i++)
+			for (int j = 0; j < w; j++)
+				cin >> map[i][j];
+
+
+
+	}
+
+	return 0;
+}
