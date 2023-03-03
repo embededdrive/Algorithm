fd0ce71862e5fcc03fbfed3dc9b81c1ae034eca8
embededdrive <embededdirve@naver.com>
2023-03-03 17:49:42 +0900

    SWEA #4014

---
+++ C++/SWEA/SWEA_#2115.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int n, m, c;
+int honey[10][10];
+
+int getMaxHoney(int y, int x)
+{
+	int sum = honey[y][x];
+	int use = 0;
+
+	for (int i = 1; i < m; i++)
+	{
+		if (x + i >= n)
+			break;
+
+		if (sum + honey[y][x + i] > c)
+			break;
+
+		sum += honey[y][x + i];
+	}
+
+	return sum;
+}
+
+int getMaxInRange(int y, int x)
+{
+
+}
+
+void input()
+{
+	cin >> n >> m >> c;
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> honey[i][j];
+}
+
+int main()
+{
+	int t;
+
+	cin >> t;
+
+	input();
+
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << getMaxHoney(i, j) << ' ';
+		}
+		cout << endl;
+	}
+	
+
+	/*for (int tc = 1; tc <= t; tc++)
+	{
+
+		
+
+		cout << '#' << tc << ' ' << ans << '\n';
+	}*/
+
+	return 0;
+}
+
+
+/*
+
+특정 좌표의 최대 채취량 구하는 함수
+
+특정 좌표로부터 마지막 좌표까지의 최대 채취량을 구하는 DP함수
+
+*/
---
+++ C++/SWEA/SWEA_#4014.cpp
@@ -0,0 +1,132 @@
+﻿#define _CRT_NO_SECURE_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, x;
+int map[20][20];
+
+bool canBuild(int idx, int dir)
+{
+	int route[20] = { 0 };
+
+	if (dir == 1)
+	{
+		int prev = map[idx][0];
+
+		for (int i = 1; i < n; i++)
+		{
+			int next = map[idx][i];
+			int dif = abs(next - prev);
+
+			if (dif > 1)
+				return false;
+
+			if (next > prev)
+			{
+				if (i < x)
+					return false;
+
+				for (int j = 0; j < x; j++)
+				{
+					if (map[idx][i - j - 1] != prev)
+						return false;
+
+					if (route[i - j - 1] != 0)
+						return false;
+				}
+			}
+			else if (next < prev)
+			{
+				if (n - i < x)
+					return false;
+
+				for (int j = 0; j < x; j++)
+				{
+					if (map[idx][i + j] != next)
+						return false;
+					
+					route[i + j] = x - j;
+				}
+			}
+
+			prev = next;
+		}
+	}
+	else {
+		int prev = map[0][idx];
+
+		for (int i = 1; i < n; i++)
+		{
+			int next = map[i][idx];
+			int dif = abs(next - prev);
+
+			if (dif > 1)
+				return false;
+
+			if (next > prev)
+			{
+				if (i < x)
+					return false;
+
+				for (int j = 0; j < x; j++)
+				{
+					if (map[i - j - 1][idx] != prev)
+						return false;
+
+					if (route[i - j - 1] != 0)
+						return false;
+				}
+			}
+			else if (next < prev)
+			{
+				if (n - i < x)
+					return false;
+
+				for (int j = 0; j < x; j++)
+				{
+					if (map[i + j][idx] != next)
+						return false;
+
+					route[i + j] = x - j;
+				}
+			}
+
+			prev = next;
+		}
+	}
+
+	return true;
+}
+
+int main()
+{
+	freopen("input.txt", "r", stdin);
+
+	int t;
+
+	cin >> t;
+
+	for (int tc = 1; tc <= t; tc++)
+	{
+		cin >> n >> x;
+
+		int ans = 0;
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				cin >> map[i][j];
+
+		
+		for (int i = 0; i < n; i++)
+			ans += canBuild(i, 0) << ' ';
+
+		for (int i = 0; i < n; i++)
+			ans += canBuild(i, 1) << ' ';
+
+		cout << '#' << tc << ' ' << ans << '\n';
+	}
+
+	return 0;
+}
